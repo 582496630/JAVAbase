c5362b1bcc750b40c898e00b6c8d2430ce20b42b
zhouyou <582496630@qq.com>
2016-10-27 16:38:39 +0800

    测试：删减文件
    Signed-off-by: zhouyou <582496630@qq.com>

--- test/src/test/Mujin.java
+++ test/src/test/Mujin.java
@@ -4,10 +4,14 @@ import java.io.PrintStream;
 public class Mujin {
 
 	public static void main(String[] args) {
+		
+		
+		
 		PrintStream out = System.out;
 		out.println("Hello World");
 		out.println("111" + args[0] + "2222" + args[1]);
 		out.println(args[2]);
+		out.println();
 	}
 
 }
